aa9195c79b1d635d5f56404707edab4f4b91aabf
Anatanoooooooooo <165888612+Anatanoooooooooo@users.noreply.github.com>
2024-12-01 14:05:19 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,23 +8,11 @@ document.addEventListener('mousemove', (e) => {
     
     trailsContainer.appendChild(trail);
 
-    // Supprimer le tracé après un certain temps
+    
     setTimeout(() => {
         trail.style.opacity = '0';
         setTimeout(() => {
             trailsContainer.removeChild(trail);
-        }, 500); // Correspond à la durée de la transition d'opacité
+        }, 500); // Durée de la transition d'opacité
     }, 100); // Temps avant de commencer à réduire l'opacité
 });
-
-function changeGradientPosition() {
-    // Générer une position aléatoire
-    const randomX = Math.random() * 100; // Valeur entre 0 et 100
-    const randomY = Math.random() * 100; // Valeur entre 0 et 100
-    
-    // Modifier le style du body
-    document.body.style.backgroundPosition = `${randomX}% ${randomY}%`;
-}
-
-// Changer la position toutes les 2 secondes
-setInterval(changeGradientPosition, 200);
